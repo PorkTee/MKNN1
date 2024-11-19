bf5823303228897bd1e73f7bef60bb20df72c534
PORK <155814828+PorkTee@users.noreply.github.com>
2024-11-19 15:48:53 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.header('Sikawat Loksutthi')
-st.image('./img/sikawat.jpg')
+#st.image('./img/sikawat.jpg')
 col1, col2, col3 = st.columns(3)
 
 with col1:
